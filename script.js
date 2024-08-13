711ee02254b1e99eed6e90402f22758048b39000
jaimezzapata <zapataval2304@gmail.com>
2024-08-13 08:51:25 -0500

    Se agregan definicion de variables

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Datos de salida */
+
+/* Datos de entrada */
+/* Es como el Scanner en Java */
+
+/* Variables JavaScript
+ *Hay tres tipos de variables (var, let y const)
+ *Las variables no tienen tipo de dato
+ *Las tres variables almacenan cualquier tipo de dato
+ */
+
+// var nombreCompleto = prompt("Ingrese su nombre: ");
+// console.log(nombreCompleto);
+
+/* Datos de proceso */
+
+var numeroUno = Number(prompt("Ingrese un numero: "));
+var numeroDos = Number(prompt("Ingrese un numero: "));
+console.log(numeroUno + numeroDos); /* Concatenación */
+
+/* Todo dato ingresado por teclado es por defecto un String 
+*Parseo de datos (conversion de datos)
+
+*/
