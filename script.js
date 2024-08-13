47066fc901c92b1f6767a72cb758bdcdb4882903
jaimezzapata <zapataval2304@gmail.com>
2024-08-13 10:04:50 -0500

    Se incluyen comparadores lógicos y ariméticos a las notas

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-/* Datos de salida */
+/* Datos de salida 
+console.log()
+*/
 
-/* Datos de entrada */
-/* Es como el Scanner en Java */
+/* Datos de entrada 
+prompt() ->  Es como el Scanner en Java
+*/
 
 /* Variables JavaScript
  *Hay tres tipos de variables (var, let y const)
@@ -12,13 +15,63 @@
 // var nombreCompleto = prompt("Ingrese su nombre: ");
 // console.log(nombreCompleto);
 
-/* Datos de proceso */
+/* Datos de proceso 
+Operaciones aritméticas y lógicas
+Suma, resta, multiplicacion, división, comparación, asignación, incremento, decremento, etc.
 
-var numeroUno = Number(prompt("Ingrese un numero: "));
-var numeroDos = Number(prompt("Ingrese un numero: "));
-console.log(numeroUno + numeroDos); /* Concatenación */
+Aritméticos:
+    Suma: valorUno + valorDos (+)
+    Resta: valorUno - valorDos (-)
+    Multiplicación: valorUno * valorDos (*)
+    División: valorUno / valorDos (/)
+
+Lógicos (Generan un resultado booleano: true o false):
+    <    -> Menor que (Excluyente)
+    >    -> Mayor que (Excluyente)
+    <=   -> Menor o igual (Incluyente)
+    >=   -> Mayor o igual (Incluyente)
+    ==   -> Comparación Simple
+    ===  -> Comparación Estricta
+    !=   -> Negación simple
+    !==  -> Negación estricta
+
+*/
+// console.log(3 < 3);
+// console.log(3 > 2);
+
+// console.log(3 <= 3);
+// console.log(3 >= 3);
+
+console.log(4 == '4');
+console.log(3 === '3');
+
+// var numeroUno = Number(prompt("Ingrese un numero: "));
+// var numeroDos = Number(prompt("Ingrese un numero: "));
+/*console.log(numeroUno + numeroDos);  Concatenación */
 
 /* Todo dato ingresado por teclado es por defecto un String 
 *Parseo de datos (conversion de datos)
 
 */
+
+// var nombreCompleto = 'Jaime Zapata'
+// var nombreCompleto = 202020202
+// console.log(nombreCompleto)
+
+// var saldoCuenta = 20000000
+// var saldoCuenta = 2000
+// console.log(saldoCuenta)
+
+// let saldoCuenta = 20000000
+// saldoCuenta = 2000
+// console.log(saldoCuenta)
+
+// const saldoCuenta = 20000000
+// saldoCuenta = 2000
+// console.log(saldoCuenta)
+
+/* Las variables de tipo var permiten sobreescritura de variable y de contenido */
+/* Las variables de tipo let no permiten sobreescritura de variable pero si de contenido */
+/* Las variables de tipo const no permiten sobreescritura de variable ni de contenido */
+
+/* Las variables de tipo let y const son el tipo de variables mas utilizado */
